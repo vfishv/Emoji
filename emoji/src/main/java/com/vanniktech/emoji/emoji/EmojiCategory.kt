44fa9a30f347d755ee2299c365bd7e862c9d3d6b
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-04-27 12:34:27 +0200

    Convert EmojiCategory interface to Kotlin. (#611)

--- emoji/src/main/java/com/vanniktech/emoji/emoji/EmojiCategory.kt
+++ emoji/src/main/java/com/vanniktech/emoji/emoji/EmojiCategory.kt
@@ -14,37 +14,37 @@
  * limitations under the License.
  *
  */
+package com.vanniktech.emoji.emoji
 
-package com.vanniktech.emoji.emoji;
-
-import androidx.annotation.DrawableRes;
-import androidx.annotation.NonNull;
-import androidx.annotation.StringRes;
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 
 /**
  * Interface for defining a category.
  *
  * @since 0.4.0
  */
-public interface EmojiCategory {
+interface EmojiCategory {
   /**
    * Returns all of the emojis it can display.
    *
    * @since 0.4.0
    */
-  @NonNull Emoji[] getEmojis();
+  val emojis: Array<Emoji>
 
   /**
    * Returns the icon of the category that should be displayed.
    *
    * @since 0.4.0
    */
-  @DrawableRes int getIcon();
+  @get:DrawableRes
+  val icon: Int
 
   /**
    * Returns category name.
    *
    * @since 0.7.0
    */
-  @StringRes int getCategoryName();
+  @get:StringRes
+  val categoryName: Int
 }
