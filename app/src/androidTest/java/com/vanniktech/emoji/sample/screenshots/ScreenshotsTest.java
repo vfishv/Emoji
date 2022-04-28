5862a8a24e2db8902db773fb060206a46c8ac686
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-04-29 00:03:04 +0200

    Refactoring: Use espresso utils, get rid of assertj & more. (#665)

--- app/src/androidTest/java/com/vanniktech/emoji/sample/screenshots/ScreenshotsTest.java
+++ app/src/androidTest/java/com/vanniktech/emoji/sample/screenshots/ScreenshotsTest.java
@@ -17,19 +17,15 @@
 
 package com.vanniktech.emoji.sample.screenshots;
 
-import android.content.Context;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 import com.vanniktech.emoji.sample.MainActivity;
 import com.vanniktech.emoji.sample.R;
-import java.io.File;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import tools.fastlane.screengrab.FileWritingScreenshotCallback;
 import tools.fastlane.screengrab.Screengrab;
 import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
 import tools.fastlane.screengrab.locale.LocaleTestRule;
@@ -38,7 +34,7 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static com.vanniktech.emoji.sample.screenshots.AppendTextAction.append;
+import static com.vanniktech.espresso.core.utils.AppendTextAction.appendText;
 import static java.util.Locale.US;
 
 @RunWith(AndroidJUnit4.class) public final class ScreenshotsTest {
@@ -70,50 +66,40 @@ import static java.util.Locale.US;
     final String name = variant.name().toLowerCase(US);
 
     // Select the right variant.
-    onView(withId(R.id.menuMainSearchOption)).perform(click());
+    onView(withId(R.id.button)).perform(click());
     onView(withText(variant.title)).perform(click());
 
     // First text.
-    onView(withId(R.id.main_activity_emoji)).perform(click());
+    onView(withId(R.id.chatEmoji)).perform(click());
 
     final int[] firstEmojis = new int[] { 0x1f913, 0x1F60E, 0x1F921, 0x1F920, 0x1F60F, 0x1F3BE };
-    onView(withId(R.id.main_activity_chat_bottom_message_edittext)).perform(append("Hello what's up? " + new String(firstEmojis, 0, firstEmojis.length)));
+    onView(withId(R.id.chatEditText)).perform(appendText("Hello what's up? " + new String(firstEmojis, 0, firstEmojis.length)));
 
     Thread.sleep(500); // Espresso does not synchronize it right away.
-    Screengrab.screenshot(name + "_1", Screengrab.getDefaultScreenshotStrategy(), new ConstantFileWritingScreenshotCallback(InstrumentationRegistry.getInstrumentation().getTargetContext()));
+    Screengrab.screenshot(name + "_1");
 
-    onView(withId(R.id.main_activity_send)).perform(click());
+    onView(withId(R.id.chatSend)).perform(click());
 
     // Second text.
     final int[] beerEmojis = new int[] { 0x1F37A, 0x1F37A, 0x1F37A };
-    onView(withId(R.id.main_activity_chat_bottom_message_edittext)).perform(append(new String(beerEmojis, 0, beerEmojis.length)));
-    onView(withId(R.id.main_activity_send)).perform(click());
+    onView(withId(R.id.chatEditText)).perform(appendText(new String(beerEmojis, 0, beerEmojis.length)));
+    onView(withId(R.id.chatSend)).perform(click());
 
     final int[] clinkingBeerEmoji = new int[] { 0x1F37B };
-    onView(withId(R.id.main_activity_chat_bottom_message_edittext)).perform(append(new String(clinkingBeerEmoji, 0, clinkingBeerEmoji.length)));
-    onView(withId(R.id.main_activity_send)).perform(click());
+    onView(withId(R.id.chatEditText)).perform(appendText(new String(clinkingBeerEmoji, 0, clinkingBeerEmoji.length)));
+    onView(withId(R.id.chatSend)).perform(click());
 
     Thread.sleep(500); // Espresso does not synchronize it right away.
-    Screengrab.screenshot(name + "_2", Screengrab.getDefaultScreenshotStrategy(), new ConstantFileWritingScreenshotCallback(InstrumentationRegistry.getInstrumentation().getTargetContext()));
+    Screengrab.screenshot(name + "_2");
 
     // Third text.
-    onView(withId(R.id.main_activity_send)).perform(click());
+    onView(withId(R.id.chatSend)).perform(click());
 
     final int[] secondEmojis = new int[] { 0x1F98B, 0x1F41E, 0x1F41D, 0x1F422, 0x1F432, 0x1F683, 0x1F37B, 0x1F943 };
-    onView(withId(R.id.main_activity_chat_bottom_message_edittext)).perform(append("I don't know " + new String(secondEmojis, 0, secondEmojis.length)));
+    onView(withId(R.id.chatEditText)).perform(appendText("I don't know " + new String(secondEmojis, 0, secondEmojis.length)));
 
     Thread.sleep(500); // Espresso does not synchronize it right away.
-    Screengrab.screenshot(name + "_3", Screengrab.getDefaultScreenshotStrategy(), new ConstantFileWritingScreenshotCallback(InstrumentationRegistry.getInstrumentation().getTargetContext()));
-  }
-
-  static class ConstantFileWritingScreenshotCallback extends FileWritingScreenshotCallback {
-    ConstantFileWritingScreenshotCallback(final Context appContext) {
-      super(appContext);
-    }
-
-    @Override protected File getScreenshotFile(final File screenshotDirectory, final String screenshotName) {
-      return new File(screenshotDirectory, screenshotName + ".png");
-    }
+    Screengrab.screenshot(name + "_3");
   }
 
   enum Variant {
